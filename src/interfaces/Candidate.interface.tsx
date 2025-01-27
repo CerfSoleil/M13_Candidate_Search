6f5bc491ee3475236358bc0abb2f22e847f1905d
CerfSoleil <robedj12@gmail.com>
2025-01-27 16:24:37 -0600

    candidateInterface created

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    readonly login: string;
+    readonly name?: string | null;
+    readonly avatar_url?: string | null;
+    readonly email?: string | null;
+    readonly html_url?: string | null;
+    readonly location?: string | null;
+    readonly company?: string | null;
+    readonly bio?: string | null;
+    readonly blog?: string | null;
+}
