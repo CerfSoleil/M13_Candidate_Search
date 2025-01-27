7e2a1a023f3bb95dd60cc91a3d490b5b9d1ea745
CerfSoleil <robedj12@gmail.com>
2025-01-27 17:48:02 -0600

    Pages Initialized

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav>
+      <div>
+        <Link to= "/">Home</Link>
+      </div>
+      <div>
+        <Link to="/SavedCandidates">Bookmarked Candidates</Link>
+      </div>
+    </nav>
   )
 };
 
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,10 +1,13 @@
-const ErrorPage = () => {
+import { Link } from 'react-router-dom';
+
+function ErrorPage() {
   return (
     <section>
       <h1>404: Page Not Found</h1>
       <h1> ¯\_(ツ)_/¯</h1>
+      <Link to="/">Return Home</Link>
     </section>
   );
-};
+}
 
 export default ErrorPage;
